e0e06f4eea4c1a60481afd59cf8ca4fd6898bf96
Gonçalo Garcia <goncalo.torrao@talkdesk.com>
2020-12-13 14:35:48 +0000

    Allow setting background color and defaultPathOpacity as props
    Since the background color was set to white, the component does not look
    good in different backgrounds. Additionaly, the default path opacity
    (0.2) is too transparent for light backgrounds.
    
    This commit exposes properties to configure those parameters.

---
+++ examples/custom-style-example/build/precache-manifest.934b42a0e58b24aa7f6d80e976eee0e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463a19a4e5014c65ddb7f389d6c17496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24a174da3fc4e87f4ba6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d59be2884bafb0102e4",
+    "url": "/static/js/2.44848df0.chunk.js"
+  },
+  {
+    "revision": "b286c92f1e391ff6cca80a9d15faa330",
+    "url": "/static/js/2.44848df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a174da3fc4e87f4ba6",
+    "url": "/static/js/main.339f0ef1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
